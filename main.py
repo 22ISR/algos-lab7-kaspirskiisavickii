59308f5f393eefb3d76e95cfe3e9968ed48e7860
Temka <artemijkaspirskij68@gmail.com>
2025-06-22 15:28:09 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,98 +1,74 @@
-"""
-Задание №1
+# Задание №1
 
-Задание: Написать программу, которая принимает на вход слово и проверяет, является ли оно палиндромом.
-Условия:
+# Задание: Написать программу, которая принимает на вход слово и проверяет, является ли оно палиндромом.
+# Условия:
 
-    Программа должна быть нечувствительна к регистру.
-    Игнорировать пробелы и знаки пунктуации.
-Пример использования:
-    isPalindrom("level") # True
-    isPalindrom("hello") # False
+#     Программа должна быть нечувствительна к регистру.
+#     Игнорировать пробелы и знаки пунктуации.
+# Пример использования:
+#     isPalindrom("level") # True
+#     isPalindrom("hello") # False
 
-Задание №2
-
-Задание: Написать программу, которая принимает список слов и проверяет, какие из них являются палиндромами.
-Условия:
-
-    Слова передаются в виде списка через ввод пользователя.
-    Программа должна вывести все палиндромы из списка.
-
-Пример использования:
-    isPalindromList(["hello", "list", "level"]) # ["level"]
-
-Задание №3
-
-Задание: Написать программу, которая ищет все палиндромы в строке текста.
-Условия:
-
-    Программа должна игнорировать регистр и знаки пунктуации.
-    Если палиндромы повторяются, выводить их только один раз.
-
-Пример использования isPalindromString("Madam, Anna went to the civic center") # ["madam", "anna", "civic"]
-"""
-
-""""
 def isPalindrom(word):
-    cleaner_word = word.replace(" ", "").lower()
-    reversed_word = cleaner_word[::-1]
-    return reversed_word == cleaner_word
-a = input("Попробуй угадать слово...")
-if isPalindrom(a):
-    print("Палиндром")
-else:
-    print("не Палиндром") """
-
-
-
-"""def isPalindrom(word):
-    cleaner_word =  word.replace(" ", " ").lower()
-    reversed_word = cleaner_word[::-1]
-    return reversed_word == cleaner_word
-
-a = list(["level", "word", "table", "music", "madam"])
-
-def isPalindromList(words):
-    Palindroms = []
-    for word in words:
-        if isPalindrom(word):
-            Palindroms.append(word)
-    return Palindroms
+    word_lower = word.lower()
     
-print(a)    
-print(isPalindromList(a))"""
+    cl_word = ""
+    for a in word_lower:
+        if a.isalnum():
+            cl_word += a
+    
+    return cl_word == cl_word[::-1]
+print(isPalindrom("level"))
+print(isPalindrom("hello"))
 
 
-"""def punctuation(string):
-    punctuation = "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
-    result = ""
+# Задание №2
 
-    for char in string:
-        if char not in punctuation:
-            result += char
+# Задание: Написать программу, которая принимает список слов и проверяет, какие из них являются палиндромами.
+# Условия:
 
-    return result
+#     Слова передаются в виде списка через ввод пользователя.
+#     Программа должна вывести все палиндромы из списка.
 
-def isPalindrom(words):
-    cleaner_word = words.replace(" ", "").lower()
-    reversed_word = cleaner_word[::-1]
-    return reversed_word == cleaner_word
+# Пример использования:
+#     isPalindromList(["hello", "list", "level"]) # ["level"]
 
-def isPalindromList(words):
-    palindroms = []
-    for word in words:
-        if isPalindrom(word):
-            palindroms.append(word)
-    return palindroms
+def is_palindromchik(word):
+    word = word.lower()
+    word = word.replace(" ", "")
+    return word == word[::-1]
 
-def isPalindromString(string):
-    clean_string = punctuation(string)
-    words = clean_string.split()
-    print(words)
-    palindromes = isPalindromList(words)
+def palindrom_list(word_list):
+    palindromes = []
+    for word in word_list:
+        if is_palindromchik(word):
+            palindromes.append(word)
     return palindromes
 
-print(isPalindromString("Madam, Anna went to the civic center"))"""
+my_words = ["hello", "list", "level"]
+print(palindrom_list(my_words))
 
+# Задание №3
 
+# Задание: Написать программу, которая ищет все палиндромы в строке текста.
+# Условия:
 
+#     Программа должна игнорировать регистр и знаки пунктуации.
+#     Если палиндромы повторяются, выводить их только один раз.
+
+# Пример использования isPalindromString("Madam, Anna went to the civic center") # ["madam", "anna", "civic"]
+
+def isPalindromString(text):
+    text = text.lower()
+    words = text.split()
+    
+    resul = []
+    for word in words:
+        clean_words= word.strip(",.!?;:-'\"")
+        if len(clean_words) > 1 and clean_words == clean_words[::-1]:
+            if clean_words not in resul:
+                resul.append(clean_words)
+    
+    return resul
+
+print(isPalindromString("Madam, Anna went to the civic center"))  
